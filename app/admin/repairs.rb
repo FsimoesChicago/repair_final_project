b5d40753bb5359795c1fda1f18657686343b771b
Raghu Betina <raghu@firstdraft.com>
2019-12-05 22:20:02 +0000

    Whitelist columns in Repair for ActiveAdmin

--- app/admin/repairs.rb
+++ app/admin/repairs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Repair do
+
+ permit_params :location, :repair_type, :price, :client_id, :service_provider_id, :car_type
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
