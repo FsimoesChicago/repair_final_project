72e56e8700a84604759ec29aa7f48ceb7f88cdd5
Raghu Betina <raghu@firstdraft.com>
2019-12-05 22:20:02 +0000

    Only repair user can modify repair

--- app/controllers/repairs_controller.rb
+++ app/controllers/repairs_controller.rb
@@ -1,4 +1,14 @@
 class RepairsController < ApplicationController
+  before_action :current_client_must_be_repair_client, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_client_must_be_repair_client
+    repair = Repair.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_client == repair.client
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @repairs = Repair.all
 
